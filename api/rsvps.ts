ea831719e4ab3dcdf869fa4f21ad19e4afbc39dc
sameershanbhag <sameer.shanbhag1995@gmail.com>
2025-11-13 22:33:45 -0800

    Add .js extension to ESM imports for Vercel compatibility

--- api/rsvps.ts
+++ api/rsvps.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getDatabase } from './lib/mongodb';
+import { getDatabase } from './lib/mongodb.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   // Enable CORS
